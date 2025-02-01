fcc5c0d824c496fc2e2a3c7b9454d6e5afc5cc8e
LordBombardir <lordanon6@gmail.com>
2025-02-01 17:04:30 +0300

    Update xmake.lua

--- packages/p/powerranks/xmake.lua
+++ packages/p/powerranks/xmake.lua
@@ -1,4 +1,4 @@
-package("translatorapi")
+package("powerranks")
     add_urls("https://github.com/LordBombardir/LLPowerRanks/releases/download/v$(version)/LLPowerRanks-windows-x64.zip")
     add_versions("1.0.3", "bc08e429d24a883b339b57a8eedbfaf2379787bb24162516e6c7df72b1762f16")
 
