e3f49047212a07641fe69e73a05012186abb401b
LordBombardir <lordanon6@gmail.com>
2025-02-26 12:23:22 +0300

    Update xmake.lua

--- packages/t/translatorapi/xmake.lua
+++ packages/t/translatorapi/xmake.lua
@@ -2,6 +2,7 @@ package("translatorapi")
     add_urls("https://github.com/LordBombardir/LLTranslatorApi/releases/download/v$(version)/LLTranslatorApi-windows-x64.zip")
     add_versions("1.0.0", "54b7beaf3944af713169d8d62d25c64eb3974ecae901a8afb819231ce0eea77b")
     add_versions("1.0.1", "351c60a4e08ce55a4b8da9bfd72d2a1cbd1216417f177ea9da2a88604e6833ad")
+    add_versions("1.1.0", "d647088f8631a51d6af559f3c46fb9b4a81ff3366cc20a47faf928ff32bce2d2")
 
 
     on_install(function (package)
