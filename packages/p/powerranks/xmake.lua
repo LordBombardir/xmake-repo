5573be1a5515d9816942d8a04e80a28eb5c5f632
LordBombardir <lordanon6@gmail.com>
2025-03-08 19:48:20 +0300

    Update xmake.lua

--- packages/p/powerranks/xmake.lua
+++ packages/p/powerranks/xmake.lua
@@ -2,6 +2,7 @@ package("powerranks")
     add_urls("https://github.com/LordBombardir/LLPowerRanks/releases/download/v$(version)/LLPowerRanks-windows-x64.zip")
     add_versions("1.0.3", "bc08e429d24a883b339b57a8eedbfaf2379787bb24162516e6c7df72b1762f16")
     add_versions("1.0.4", "cf71c0344d469cb366ffc755ee9d8884a6a4a1041b1cb7126a6370cdb4ebd74b")
+    add_versions("1.0.5", "af6387e51243e2c28c7f1360294d89be870d32b59f2bee201dd2b00ab028ce3a")
 
     on_install(function (package)
         os.cp("include", package:installdir())
