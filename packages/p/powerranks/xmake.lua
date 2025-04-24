46a8d6d9fe7b0fb05b20ad0bd9449cd7696a282b
LordBombardir <lordanon6@gmail.com>
2025-04-24 16:11:52 +0300

    Update xmake.lua

--- packages/p/powerranks/xmake.lua
+++ packages/p/powerranks/xmake.lua
@@ -4,7 +4,7 @@ package("powerranks")
     add_versions("1.0.4", "cf71c0344d469cb366ffc755ee9d8884a6a4a1041b1cb7126a6370cdb4ebd74b")
     add_versions("1.0.5", "af6387e51243e2c28c7f1360294d89be870d32b59f2bee201dd2b00ab028ce3a")
     add_versions("1.0.6", "d053c0eaf2f0d47bff6e4209a68ec1b375b3193d03bb788f6f5a44528d9f5d14")
-    add_versions("1.0.7", "a1f5b12f667384936eb9b0ced7c779f322040735c92ae98a381f7fde5b79770d")
+    add_versions("1.0.7", "77b1fe38b97b0b1aee4c1ac6aab95c89e9614f1ef08588a83453c3a1f6f5d52c")
 
     on_install(function (package)
         os.cp("include", package:installdir())
