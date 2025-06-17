258171096d3e1c9dfc5615bc82bbb2adcfa9ef75
LordBombardir <lordanon6@gmail.com>
2025-06-17 18:13:10 +0300

    Update xmake.lua

--- packages/p/playerdb/xmake.lua
+++ packages/p/playerdb/xmake.lua
@@ -1,4 +1,4 @@
-package("powerranks")
+package("playerdb")
     add_urls("https://github.com/LordBombardir/LLPlayerDB-Release/releases/download/v$(version)/LLPlayerDB-windows-x64.zip")
     add_versions("1.0.0", "4fe648a98821ea9ee6d66ebc7dacbc8b985783ac0165c0eb47b0e93558dcbf97")
 
