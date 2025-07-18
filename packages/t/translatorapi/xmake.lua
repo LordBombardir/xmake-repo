1e6fe15bcecad34c8abcab509d039dbbb4a493b4
LordBombardir <lordanon6@gmail.com>
2025-07-18 11:11:32 +0300

    Update xmake.lua

--- packages/t/translatorapi/xmake.lua
+++ packages/t/translatorapi/xmake.lua
@@ -6,6 +6,7 @@ package("translatorapi")
     add_versions("1.1.1", "3c5b84f4bb283cec77b3f85023ea23884362c126a94883550817fdc6e33fed27")
     add_versions("1.1.2", "b388ee099a21be682ad81788356b7c8a5430a40a205316dacda6048e94683c7a")
     add_versions("1.1.3", "54ff24431f5c096ac53162947dae4f2c446ef4e2e95ffc0acd830c73d747244e")
+    add_versions("1.1.5", "26932d9079bef9ac80b828dc2806768a89c65cb9f1eb8d581c69ad7fc5ef256d")
 
     on_install(function (package)
         os.cp("include", package:installdir())
