c3f53aa488bd5be71e38b1c752c7cffafe7c5abe
LordBombardir <lordanon6@gmail.com>
2025-10-13 21:35:02 +0300

    Update xmake.lua

--- packages/p/playerdb/xmake.lua
+++ packages/p/playerdb/xmake.lua
@@ -2,6 +2,7 @@ package("playerdb")
     add_urls("https://github.com/LordBombardir/LLPlayerDB-Release/releases/download/v$(version)/LLPlayerDB-windows-x64.zip")
     add_versions("1.0.0", "affb7d22763ba1c85332c3e7d295ae4c93b60a4029d758c8704d614d3c7b3e3b")
     add_versions("1.0.1", "971434662a21bebdd8165077a9731333d43eaee044b5628462194168a1590ed8")
+    add_versions("1.1.0", "31c91508b2676015bad7b42af232ee8e3821b53e267a616266313692682080e1")
 
     on_install(function (package)
         os.cp("include", package:installdir())
