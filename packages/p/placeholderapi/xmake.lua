a1da8458c748367198223f86598a5080bf2ed9ce
LordBombardir <lordanon6@gmail.com>
2026-02-04 13:00:21 +0300

    Update xmake.lua

--- packages/p/placeholderapi/xmake.lua
+++ packages/p/placeholderapi/xmake.lua
@@ -18,6 +18,11 @@ package("placeholderapi")
         ["1.0.0-server-debug"]   = "2d04d4935220ec14bcc9c5922a9ba4e52000838e83606e366ddf14c582c187bd",
         ["1.0.0-client-release"] = "f9218e9beff323e84bac1b15a4508b574d43995798c62582274f050498693ed5",
         ["1.0.0-client-debug"]   = "e97456e8755920e942893110bf3953b66227fb171404c7ead1841206fa3fde40",
+
+        ["1.0.1-server-release"] = "50291eef1c60937fefd66c59b49f8110c9e750262524a8bd57e5164013e92da6",
+        ["1.0.1-server-debug"]   = "a9d9d0cf9dd56f802004bbbd2164ffeefb12bea8e247574cab826cf2dabb05ef",
+        ["1.0.1-client-release"] = "0649df916831d3faf15cb6fd6217ed93f0e2d7cd9b67a1aba67542771182ec40",
+        ["1.0.1-client-debug"]   = "a3c393b6d7c2751dc8ba9659c75aaf658fa30f7ba822a02f7abaa683086df53f",
     }
 
     on_load(function (package)
