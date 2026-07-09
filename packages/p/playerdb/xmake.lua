3e8eac1658aece549d6735d499ccb7aec79d5d19
LordBombardir <lordanon6@gmail.com>
2026-07-09 11:07:36 +0300

    Update xmake.lua

--- packages/p/playerdb/xmake.lua
+++ packages/p/playerdb/xmake.lua
@@ -1,8 +1,33 @@
 package("playerdb")
-    add_urls("https://github.com/LordBombardir/LLPlayerDB-Release/releases/download/v$(version)/LLPlayerDB-windows-x64.zip")
-    add_versions("1.0.0", "affb7d22763ba1c85332c3e7d295ae4c93b60a4029d758c8704d614d3c7b3e3b")
-    add_versions("1.0.1", "971434662a21bebdd8165077a9731333d43eaee044b5628462194168a1590ed8")
-    add_versions("1.1.0", "31c91508b2676015bad7b42af232ee8e3821b53e267a616266313692682080e1")
+    add_configs("target_type", {
+        description = "Mod target type",
+        default = "server",
+        values = {"server", "client"}
+    })
+
+    add_configs("mode", {
+        description = "Build mode",
+        default = "release",
+        values = {"release", "debug"}
+    })
+    
+    add_urls("https://github.com/LordBombardir/LLPlayerDB-Release.git")
+
+    local shas = {    
+        ["26.20.0-server-release"] = "0f2f9e3cc3c5616af6e5b7fc1879c711eadc849e9465028143ef9d3626686930",
+        ["26.20.0-client-release"] = "bedfa438e96908d0b116aa781ea03efb0689b21a45f3adf49732621ee57246f4",
+    }
+
+    on_load(function (package)
+        local base_ver = package:version_str()
+        local tt = package:config("target_type") or "server"
+        local mode = package:config("mode") or "release"
+        local ver = base_ver .. "-" .. tt .. "-" .. mode
+
+        package:set("version", ver)
+        package:add("urls", ("https://github.com/LordBombardir/LLPlayerDB-Release/releases/download/v%s/LLPlayerDB-v%s-%s-%s-windows-x64.zip"):format(base_ver, base_ver, tt, mode))
+        package:add("hashes", shas[ver])
+    end)
 
     on_install(function (package)
         os.cp("include", package:installdir())
